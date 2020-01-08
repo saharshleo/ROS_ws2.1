40c6451b22ddaeaa1797206999ff7232aee280ed
saharshleo <saharshjain2000@gmail.com>
2020-01-08 18:47:40 +0530

    installation guide

---
+++ ros_installation.sh
@@ -0,0 +1,22 @@
+# first run this commands in terminal
+# sudo chmod +x ros_installation.sh
+# ./ros_installation.sh
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+sudo apt-get update
+
+sudo apt-get install ros-kinetic-desktop-full
+
+sudo rosdep init
+
+rosdep update
+
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
+
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
